24c6475b5c6d1213d11a2fa04a7219f96680614a
asenmomchev <asen.momchev.a60@learn.telerikacademy.com>
2024-05-15 17:46:29 +0300

    Implement ListTasks command

---
+++ src/com/practice/projectone/teammanagement/commands/listing/ListTasksCommand.java
@@ -0,0 +1,59 @@
+package com.practice.projectone.teammanagement.commands.listing;
+
+import com.practice.projectone.teammanagement.commands.BaseCommand;
+import com.practice.projectone.teammanagement.core.contracts.TeamRepository;
+import com.practice.projectone.teammanagement.models.contracts.Board;
+import com.practice.projectone.teammanagement.models.contracts.Task;
+import com.practice.projectone.teammanagement.models.contracts.Team;
+import com.practice.projectone.teammanagement.utils.ValidationHelpers;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ListTasksCommand extends BaseCommand {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+    public ListTasksCommand(TeamRepository teamRepository) {
+        super(teamRepository);
+    }
+
+    @Override
+    public String execute(List<String> parameters) {
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+        String sort = parameters.get(0);
+        String filter = parameters.get(1);
+        return listTasks(sort, filter);
+    }
+
+    private String listTasks(String sort, String filter) {
+        StringBuilder builder = new StringBuilder();
+        List<Team> teams = getTeamRepository().getTeams();
+        List<Board> boards = teams.stream().flatMap(team -> team.getBoards().stream()).toList();
+        if (sort.equalsIgnoreCase("nosort") && filter.equalsIgnoreCase("nofilter")){
+            boards
+                    .stream()
+                    .flatMap(board -> board.getTasks().stream())
+                    .forEach(builder::append);
+        } else if (sort.equalsIgnoreCase("title") && filter.equalsIgnoreCase("nofilter")){
+            boards
+                    .stream()
+                    .flatMap(board -> board.getTasks().stream())
+                    .sorted(Comparator.comparing(Task::getName))
+                    .forEach(builder::append);
+        } else if (sort.equalsIgnoreCase("title") && !filter.equalsIgnoreCase("nofilter")){
+            boards
+                    .stream()
+                    .flatMap(board -> board.getTasks().stream())
+                    .filter(task -> task.getName().equals(filter))
+                    .sorted(Comparator.comparing(Task::getName))
+                    .forEach(builder::append);
+        } else {
+            boards
+                    .stream()
+                    .flatMap(board -> board.getTasks().stream())
+                    .filter(task -> task.getName().equals(filter))
+                    .forEach(builder::append);
+        }
+        return builder.toString();
+    }
+}
