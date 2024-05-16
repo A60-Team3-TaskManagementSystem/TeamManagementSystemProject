c5288f0850ce66962de721b9b9588a26c92ce2e0
asenmomchev <asen.momchev.a60@learn.telerikacademy.com>
2024-05-16 16:48:56 +0300

    Implement ListStories command

--- src/com/practice/projectone/teammanagement/models/contracts/Story.java
+++ src/com/practice/projectone/teammanagement/models/contracts/Story.java
@@ -2,6 +2,6 @@ package com.practice.projectone.teammanagement.models.contracts;
 
 import com.practice.projectone.teammanagement.models.enums.Size;
 
-public interface Story extends Task{
+public interface Story extends Task, Assignable, Prioritizable{
     Size getSize();
 }
