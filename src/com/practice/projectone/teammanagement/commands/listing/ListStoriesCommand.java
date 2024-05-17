cb3448fd8517c1319c6d024106a7aa5c271e7756
asenmomchev <asen.momchev.a60@learn.telerikacademy.com>
2024-05-17 14:11:36 +0300

    Implement new ListStories command

--- src/com/practice/projectone/teammanagement/commands/listing/ListStoriesCommand.java
+++ src/com/practice/projectone/teammanagement/commands/listing/ListStoriesCommand.java
@@ -3,15 +3,14 @@ package com.practice.projectone.teammanagement.commands.listing;
 import com.practice.projectone.teammanagement.commands.BaseCommand;
 import com.practice.projectone.teammanagement.core.contracts.TeamRepository;
 import com.practice.projectone.teammanagement.models.tasks.contracts.Story;
-import com.practice.projectone.teammanagement.models.tasks.enums.Status;
-import com.practice.projectone.teammanagement.utils.ValidationHelpers;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListStoriesCommand extends BaseCommand {
 
-    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 3;
+    public static final String INVALID_SORT_PARAMETER = "Invalid sorting parameter: should be \"title\", \"priority\", \"size\" or \"nosort\"!";
 
     public ListStoriesCommand(TeamRepository teamRepository) {
         super(teamRepository);
@@ -19,123 +18,145 @@ public class ListStoriesCommand extends BaseCommand {
 
     @Override
     public String execute(List<String> parameters) {
-        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
-        String sort = parameters.get(0);
-        String filter1 = parameters.get(1);
-        String filter2 = parameters.get(2);
-        return listStories(sort, filter1, filter2);
+        if (parameters.size() > 3){
+            throw new IllegalArgumentException("Argument count should be 3 or fewer!");
+        }
+        if (parameters.size() == 1){
+            String sort = parameters.get(0);
+            return listStories(sort);
+        } else if (parameters.size() == 2) {
+            String sort = parameters.get(0);
+            String filter1 = parameters.get(1);
+            return listStories(sort, filter1);
+        } else if (parameters.size() == 3) {
+            String sort = parameters.get(0);
+            String filter1 = parameters.get(1);
+            String filter2 = parameters.get(2);
+            return listStories(sort, filter1, filter2);
+        }
+        return listAllStories();
+    }
+
+    private String listStories(String sort) {
+        String result;
+        switch (sort) {
+            case "title":
+                result = getTeamRepository().getStories()
+                        .stream()
+                        .sorted(Comparator.comparing(Story::getName))
+                        .map(Story::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+                break;
+            case "priority":
+                result = getTeamRepository().getStories()
+                        .stream()
+                        .sorted(Comparator.comparing(Story::getPriority))
+                        .map(Story::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+                break;
+            case "size":
+                result = getTeamRepository().getStories()
+                        .stream()
+                        .sorted(Comparator.comparing(Story::getSize))
+                        .map(Story::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+                break;
+            case "nosort":
+                return listAllStories();
+            default:
+                throw new IllegalArgumentException(INVALID_SORT_PARAMETER);
+        }
+        return result;
+    }
+
+    private String listStories(String sort, String filter1) {
+        String result;
+        switch (sort) {
+            case "title":
+                result = getTeamRepository().getStories()
+                        .stream()
+                        .filter(story -> story.getAssignee().equals(filter1) || story.getStatus().toString().equals(filter1))
+                        .sorted(Comparator.comparing(Story::getName))
+                        .map(Story::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+                break;
+            case "priority":
+                result = getTeamRepository().getStories()
+                        .stream()
+                        .filter(story -> story.getAssignee().equals(filter1) || story.getStatus().toString().equals(filter1))
+                        .sorted(Comparator.comparing(Story::getPriority))
+                        .map(Story::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+                break;
+            case "size":
+                result = getTeamRepository().getStories()
+                        .stream()
+                        .filter(story -> story.getAssignee().equals(filter1) || story.getStatus().toString().equals(filter1))
+                        .sorted(Comparator.comparing(Story::getSize))
+                        .map(Story::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+                break;
+            case "nosort":
+                result = getTeamRepository().getStories()
+                        .stream()
+                        .filter(story -> story.getAssignee().equals(filter1) || story.getStatus().toString().equals(filter1))
+                        .map(Story::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+                break;
+            default:
+                throw new IllegalArgumentException(INVALID_SORT_PARAMETER);
+        }
+        return result;
     }
 
     private String listStories(String sort, String filter1, String filter2) {
-        StringBuilder builder = new StringBuilder();
-        List<Story> stories = getTeamRepository().getStories();
-        if (sort.equalsIgnoreCase("nosort")) {
-            if (filter1.equalsIgnoreCase("nofilter")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("notdone")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.NOT_DONE).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.NOT_DONE).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("inprogress")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.IN_PROGRESS).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.IN_PROGRESS).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("done")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.DONE).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.DONE).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            }
-        } else if (sort.equalsIgnoreCase("title")) {
-            if (filter1.equalsIgnoreCase("nofilter")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().sorted(Comparator.comparing(Story::getName)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getAssignee().equals(filter2)).sorted(Comparator.comparing(Story::getName)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("notdone")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.NOT_DONE).sorted(Comparator.comparing(Story::getName)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.NOT_DONE).sorted(Comparator.comparing(Story::getName)).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("inprogress")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.IN_PROGRESS).sorted(Comparator.comparing(Story::getName)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.IN_PROGRESS).sorted(Comparator.comparing(Story::getName)).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("done")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.DONE).sorted(Comparator.comparing(Story::getName)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.DONE).sorted(Comparator.comparing(Story::getName)).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            }
-        } else if (sort.equalsIgnoreCase("priority")) {
-            if (filter1.equalsIgnoreCase("nofilter")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().sorted(Comparator.comparing(Story::getPriority)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getAssignee().equals(filter2)).sorted(Comparator.comparing(Story::getPriority)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("notdone")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.NOT_DONE).sorted(Comparator.comparing(Story::getPriority)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.NOT_DONE).sorted(Comparator.comparing(Story::getPriority)).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("inprogress")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.IN_PROGRESS).sorted(Comparator.comparing(Story::getPriority)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.IN_PROGRESS).sorted(Comparator.comparing(Story::getPriority)).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("done")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.DONE).sorted(Comparator.comparing(Story::getPriority)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.DONE).sorted(Comparator.comparing(Story::getPriority)).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            }
-        } else if (sort.equalsIgnoreCase("size")) {
-            if (filter1.equalsIgnoreCase("nofilter")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().sorted(Comparator.comparing(Story::getSize)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getAssignee().equals(filter2)).sorted(Comparator.comparing(Story::getSize)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("notdone")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.NOT_DONE).sorted(Comparator.comparing(Story::getSize)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.NOT_DONE).sorted(Comparator.comparing(Story::getSize)).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("inprogress")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.IN_PROGRESS).sorted(Comparator.comparing(Story::getSize)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.IN_PROGRESS).sorted(Comparator.comparing(Story::getSize)).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            } else if (filter1.equalsIgnoreCase("done")) {
-                if (filter2.equalsIgnoreCase("nofilter")) {
-                    stories.stream().filter(story -> story.getStatus() == Status.DONE).sorted(Comparator.comparing(Story::getSize)).forEach(builder::append);
-                } else {
-                    stories.stream().filter(story -> story.getStatus() == Status.DONE).sorted(Comparator.comparing(Story::getSize)).filter(story -> story.getAssignee().equals(filter2)).forEach(builder::append);
-                }
-            }
-        } else {
-            throw new IllegalArgumentException("Invalid sorting parameter: should be \"title\", \"priority\", \"size\" or \"nosort\"");
+        String result;
+        switch (sort) {
+            case "title":
+                result = getTeamRepository().getStories()
+                        .stream()
+                        .filter(story -> story.getAssignee().equals(filter1))
+                        .filter(story -> story.getStatus().toString().equals(filter2))
+                        .sorted(Comparator.comparing(Story::getName))
+                        .map(Story::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+                break;
+            case "priority":
+                result = getTeamRepository().getStories()
+                        .stream()
+                        .filter(story -> story.getAssignee().equals(filter1))
+                        .filter(story -> story.getStatus().toString().equals(filter2))
+                        .sorted(Comparator.comparing(Story::getPriority))
+                        .map(Story::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+                break;
+            case "size":
+                result = getTeamRepository().getStories()
+                        .stream()
+                        .filter(story -> story.getAssignee().equals(filter1))
+                        .filter(story -> story.getStatus().toString().equals(filter2))
+                        .sorted(Comparator.comparing(Story::getSize))
+                        .map(Story::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+                break;
+            case "nosort":
+                result = getTeamRepository().getStories()
+                        .stream()
+                        .filter(story -> story.getAssignee().equals(filter1))
+                        .filter(story -> story.getStatus().toString().equals(filter2))
+                        .map(Story::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+                break;
+            default:
+                throw new IllegalArgumentException(INVALID_SORT_PARAMETER);
         }
-        return builder.toString();
+        return result;
+    }
+
+    private String listAllStories() {
+        return getTeamRepository().getStories()
+                .stream()
+                .map(Story::toString)
+                .collect(Collectors.joining(System.lineSeparator()));
     }
 }
