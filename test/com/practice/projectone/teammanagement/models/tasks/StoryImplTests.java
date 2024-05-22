75ed07f59f3d4a069a9ce559a28e5c38b75c923d
Milan Statev <milan_statev@yahoo.co.uk>
2024-05-22 22:23:34 +0300

    Add additional test constants

--- test/com/practice/projectone/teammanagement/models/tasks/StoryImplTests.java
+++ test/com/practice/projectone/teammanagement/models/tasks/StoryImplTests.java
@@ -1,4 +1,53 @@
 package com.practice.projectone.teammanagement.models.tasks;
 
+import com.practice.projectone.teammanagement.models.tasks.contracts.Story;
+import com.practice.projectone.teammanagement.models.tasks.contracts.Task;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.practice.projectone.teammanagement.utils.Constants.*;
+
 public class StoryImplTests {
+
+    @Test
+    public void carImpl_Should_ImplementCarInterface() {
+        StoryImpl story = initializeTestStory();
+
+        Assertions.assertTrue(story instanceof Story);
+    }
+
+    @Test
+    public void carImpl_Should_ImplementVehicleInterface() {
+        StoryImpl story = initializeTestStory();
+
+        Assertions.assertTrue(story instanceof Task);
+    }
+
+    @Test
+    public void constructor_Should_ThrowException_When_StoryTitleLengthOutOfBounds() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new StoryImpl(
+                        INVALID_TITLE_MIN,
+                        VALID_DESCRIPTION,
+                        VALID_SPECIFIC_TASK_PRIORITY,
+                        VALID_STORY_SIZE));
+    }
+
+    @Test
+    public void constructor_Should_ThrowException_When_StoryDescriptionLengthOutOfBounds() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new StoryImpl(
+                        VALID_TITLE,
+                        INVALID_DESCRIPTION_MIN,
+                        VALID_SPECIFIC_TASK_PRIORITY,
+                        VALID_STORY_SIZE));
+    }
+
+
+    public StoryImpl initializeTestStory() {
+        return new StoryImpl(VALID_TITLE,
+                VALID_DESCRIPTION,
+                VALID_SPECIFIC_TASK_PRIORITY,
+                VALID_STORY_SIZE);
+    }
 }
--- test/com/practice/projectone/teammanagement/utils/Constants.java
+++ test/com/practice/projectone/teammanagement/utils/Constants.java
@@ -1,14 +1,23 @@
 package com.practice.projectone.teammanagement.utils;
 
+import com.practice.projectone.teammanagement.models.tasks.enums.Priority;
+import com.practice.projectone.teammanagement.models.tasks.enums.Size;
+
 public class Constants {
     public static final int TITLE_LEN_MIN = 10;
+    public static final int TITLE_LEN_MAX = 100;
     public static final int DESCRIPTION_LEN_MIN = 10;
+    public static final int DESCRIPTION_LEN_MAX = 500;
     public static final int RATING_MIN_VALUE = 0;
 
     public static final String VALID_TITLE = TestUtilities.getString(TITLE_LEN_MIN + 1);
     public static final String VALID_DESCRIPTION = TestUtilities.getString(DESCRIPTION_LEN_MIN + 1);
     public static final int VALID_RATING = RATING_MIN_VALUE + 1;
-    public static final String INVALID_TITLE = TestUtilities.getString(TITLE_LEN_MIN - 1);
-    public static final String INVALID_DESCRIPTION = TestUtilities.getString(DESCRIPTION_LEN_MIN - 1);
+    public static final Size VALID_STORY_SIZE = Size.MEDIUM;
+    public static final Priority VALID_SPECIFIC_TASK_PRIORITY = Priority.MEDIUM;
+    public static final String INVALID_TITLE_MIN = TestUtilities.getString(TITLE_LEN_MIN - 1);
+    public static final String INVALID_TITLE_MAX = TestUtilities.getString(TITLE_LEN_MAX + 1);
+    public static final String INVALID_DESCRIPTION_MIN = TestUtilities.getString(DESCRIPTION_LEN_MIN - 1);
+    public static final String INVALID_DESCRIPTION_MAX = TestUtilities.getString(DESCRIPTION_LEN_MAX + 1);
     public static final int INVALID_RATING = RATING_MIN_VALUE - 1;
 }
